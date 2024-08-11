d6452be6e90458ec76674c5c1bb73d77a5702b08
Kota Hashihama <kotahashihama@gmail.com>
2024-08-11 21:14:49 +0900

    feat(Example): 例文の画像を生成

--- src/components/ExampleSection/ExampleSection.tsx
+++ src/components/ExampleSection/ExampleSection.tsx
@@ -12,10 +12,12 @@ export const ExampleSection = () => {
     生徒に楽しく英語を学んでもらえるよう、お笑い芸人の経験を活かし日々の授業に様々な工夫を凝らしています。
   `;
   const userPrompt = `
-    生徒に「${searchWord}」という単語の使い方を教えるために、ジョークを交えた面白い例文を考えてください。
-    ただし、あなたの生徒は英語が母国語ではないため、単語の語源や歴史を元にしたジョークは通じません。
-    また、例文の内容は生徒が理解しやすいものである必要があります。あまりに意図が不明なジョークや学術性の高いジョークは避けてください。
-    日本語でも通じるジョークを英訳したときに該当の単語が含まれている、という状態が理想です。
+    生徒に「${searchWord}」という単語の使い方を教えるために、冗談を交えた面白い例文を考えてください。
+    ただし、あなたの生徒は英語が母国語ではないため単語の語源や歴史を元にした冗談は通じません。
+    そのため、英文から考えるのではなくて日本語の文章を英訳する形で例文を考えるといいでしょう。
+    英訳の際は必ず「${searchWord}」を含めるようにしてください。
+    「${searchWord}」の日本語訳としてカタカナ英語や「${searchWord}」をそのまま使うのは避けてください。
+    例文の内容は生徒が理解しやすいものである必要があります。あまりに意図が不明な冗談や学術性の高い冗談は避けてください。
 
     回答は以下の形式を厳守してください。
     なお、Markdown のコードブロック等で囲む必要はなく、純粋な JSON 形式で回答してください。
@@ -26,7 +28,7 @@ export const ExampleSection = () => {
     }
   `;
 
-  const { data: exampleSentenceData, refetch } =
+  const { data: exampleSentenceData, refetch: refetchExampleSentenceData } =
     useQuery<OpenAI.Chat.Completions.ChatCompletion>({
       queryKey: ['exampleSentence', searchWord],
       queryFn: async () => {
@@ -40,6 +42,22 @@ export const ExampleSection = () => {
       },
       enabled: false,
     });
+  const {
+    data: exampleSentenceImageData,
+    refetch: refetchExampleSentenceImageData,
+  } = useQuery<OpenAI.Images.ImagesResponse>({
+    queryKey: ['exampleSentenceImage', searchWord],
+    queryFn: async () => {
+      return await openAi.images.generate({
+        model: 'dall-e-3',
+        prompt: `"I wanted to play catch with my dog, so I went outside to mete out the rules: no cheating, no barking at the neighbors, and most importantly, no stealing my snacks!" を表現する愉快な画像を生成してください。`,
+        size: '1024x1024',
+        quality: 'standard',
+        n: 1,
+      });
+    },
+    enabled: false,
+  });
 
   const hasNoMeaningData =
     !wordMeaningData ||
@@ -54,7 +72,8 @@ export const ExampleSection = () => {
         variant="contained"
         sx={{ marginBottom: '20px' }}
         onClick={() => {
-          refetch();
+          refetchExampleSentenceData();
+          refetchExampleSentenceImageData();
         }}
       >
         例文を生成
@@ -66,6 +85,7 @@ export const ExampleSection = () => {
     en: string;
     ja: string;
   } = JSON.parse(exampleSentenceData?.choices[0].message.content || '{}');
+  const imageUrl = exampleSentenceImageData?.data[0].url;
 
   return (
     <>
@@ -73,7 +93,8 @@ export const ExampleSection = () => {
         variant="contained"
         sx={{ marginBottom: '20px' }}
         onClick={() => {
-          refetch();
+          refetchExampleSentenceData();
+          refetchExampleSentenceImageData();
         }}
       >
         例文を生成
@@ -95,7 +116,7 @@ export const ExampleSection = () => {
           </Box>
 
           <Box>
-            <img src="./example.webp" style={{ maxWidth: '100%' }}></img>
+            <img src={imageUrl} style={{ maxWidth: '100%' }}></img>
           </Box>
         </Box>
       </Paper>

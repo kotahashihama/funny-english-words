46e84beb6dafbd0829bbf30e9bf9f90738f40f5a
Kota Hashihama <kotahashihama@gmail.com>
2024-08-11 16:39:31 +0900

    feat(App): ランダムサジェスト部分に Random Word API を繋ぎこみ

--- src/App.tsx
+++ src/App.tsx
@@ -1,149 +1,139 @@
-import {
-  Box,
-  createTheme,
-  Paper,
-  TextField,
-  ThemeProvider,
-  Typography,
-} from '@mui/material';
+import { Box, Paper, TextField, Typography } from '@mui/material';
 
 import Button from '@mui/material/Button';
-
-const theme = createTheme({
-  typography: {
-    button: {
-      textTransform: 'none',
-    },
-  },
-});
+import { useQuery } from '@tanstack/react-query';
 
 function App() {
+  const { data: randomWords } = useQuery({
+    queryKey: ['randomWords'],
+    queryFn: () =>
+      fetch('https://random-word-api.herokuapp.com/word?number=5').then((res) =>
+        res.json()
+      ),
+  });
+
   return (
-    <ThemeProvider theme={theme}>
-      <Box sx={{ margin: '100px auto 0', width: '680px' }}>
-        <Box
+    <Box sx={{ margin: '100px auto 0', width: '680px' }}>
+      <Box
+        sx={{
+          marginBottom: '48px',
+        }}
+      >
+        <Typography
+          fontSize="48px"
           sx={{
-            marginBottom: '48px',
+            marginBottom: '28px',
+            textAlign: 'center',
+            fontFamily: 'Ribeye Marrow, serif',
           }}
         >
-          <Typography
-            fontSize="48px"
-            sx={{
-              marginBottom: '28px',
-              textAlign: 'center',
-              fontFamily: 'Ribeye Marrow, serif',
-            }}
-          >
-            Funny English Words
-          </Typography>
+          Funny English Words
+        </Typography>
 
-          <Box
+        <Box
+          sx={{
+            display: 'flex',
+            marginBottom: '32px',
+            width: '100%',
+            justifyContent: 'center',
+          }}
+        >
+          <TextField
+            id="standard-basic"
+            label="単語を入力"
             sx={{
-              display: 'flex',
-              marginBottom: '32px',
-              width: '100%',
-              justifyContent: 'center',
+              marginRight: '16px',
+              width: '400px',
+              '& .MuiInputBase-input': {
+                background: '#fff',
+              },
             }}
-          >
-            <TextField
-              id="standard-basic"
-              label="単語を入力"
-              sx={{
-                marginRight: '16px',
-                width: '400px',
-                '& .MuiInputBase-input': {
-                  background: '#fff',
-                },
-              }}
-            />
-            <Button variant="contained">検索</Button>
-          </Box>
-
-          <Box
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              gap: '8px',
-              textAlign: 'center',
-            }}
-          >
-            <Button>applaud</Button>
-            <Button>immigrate</Button>
-            <Button>mirror</Button>
-            <Button>foodie</Button>
-            <Button>sophisticated</Button>
-          </Box>
+          />
+          <Button variant="contained">検索</Button>
         </Box>
 
-        <Paper sx={{ marginBottom: '48px', padding: '24px' }}>
-          <Typography
-            fontSize="24px"
-            fontWeight="bold"
-            sx={{ marginBottom: '20px' }}
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            gap: '8px',
+            textAlign: 'center',
+          }}
+        >
+          {randomWords?.map((word: string) => (
+            <Button key={word}>{word}</Button>
+          ))}
+        </Box>
+      </Box>
+
+      <Paper sx={{ marginBottom: '48px', padding: '24px' }}>
+        <Typography
+          fontSize="24px"
+          fontWeight="bold"
+          sx={{ marginBottom: '20px' }}
+        >
+          applaud とは
+        </Typography>
+
+        <Box fontSize="14px">
+          <Box
+            sx={{
+              marginBottom: '20px',
+            }}
           >
-            applaud とは
-          </Typography>
-
-          <Box fontSize="14px">
-            <Box
-              sx={{
-                marginBottom: '20px',
-              }}
-            >
-              applaud 【動詞】
-            </Box>
-
-            <Box sx={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
-              <Box>
-                <Box>自動:</Box>
-                <Box>拍手［称賛］する</Box>
-              </Box>
-
-              <Box>
-                <Box>他動:</Box>
-                <Box>〔～に〕拍手を送る、〔～を〕称賛する</Box>
-              </Box>
-            </Box>
+            applaud 【動詞】
           </Box>
-        </Paper>
 
-        <Button variant="contained" sx={{ marginBottom: '20px' }}>
-          例文を生成
-        </Button>
-
-        <Paper sx={{ padding: '24px' }}>
-          <Typography
-            fontSize="24px"
-            fontWeight="bold"
-            sx={{ marginBottom: '20px' }}
-          >
-            例文
-          </Typography>
-
-          <Box fontSize="14px">
-            <Box sx={{ marginBottom: '20px' }}>
-              <Box sx={{ marginBottom: '20px' }}>
-                The city's squirrels learned to{' '}
-                <span style={{ color: '#dd4e4e' }}>
-                  <b>applaud</b>
-                </span>{' '}
-                by clapping acorns together, leading to standing ovations for
-                particularly impressive parkour performances among the trees.
-              </Box>
-
-              <Box>
-                訳:
-                街のリスたちがドングリをパンパンして拍手する技を覚えちゃってさ。木の間を飛び回る超絶パルクールに大喝采するようになったんだって。
-              </Box>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+            <Box>
+              <Box>自動:</Box>
+              <Box>拍手［称賛］する</Box>
             </Box>
 
             <Box>
-              <img src="./example.webp" style={{ maxWidth: '100%' }}></img>
+              <Box>他動:</Box>
+              <Box>〔～に〕拍手を送る、〔～を〕称賛する</Box>
             </Box>
           </Box>
-        </Paper>
-      </Box>
-    </ThemeProvider>
+        </Box>
+      </Paper>
+
+      <Button variant="contained" sx={{ marginBottom: '20px' }}>
+        例文を生成
+      </Button>
+
+      <Paper sx={{ padding: '24px' }}>
+        <Typography
+          fontSize="24px"
+          fontWeight="bold"
+          sx={{ marginBottom: '20px' }}
+        >
+          例文
+        </Typography>
+
+        <Box fontSize="14px">
+          <Box sx={{ marginBottom: '20px' }}>
+            <Box sx={{ marginBottom: '20px' }}>
+              The city's squirrels learned to{' '}
+              <span style={{ color: '#dd4e4e' }}>
+                <b>applaud</b>
+              </span>{' '}
+              by clapping acorns together, leading to standing ovations for
+              particularly impressive parkour performances among the trees.
+            </Box>
+
+            <Box>
+              訳:
+              街のリスたちがドングリをパンパンして拍手する技を覚えちゃってさ。木の間を飛び回る超絶パルクールに大喝采するようになったんだって。
+            </Box>
+          </Box>
+
+          <Box>
+            <img src="./example.webp" style={{ maxWidth: '100%' }}></img>
+          </Box>
+        </Box>
+      </Paper>
+    </Box>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
+import { createTheme, ThemeProvider } from '@mui/material';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+const queryClient = new QueryClient();
+const theme = createTheme({
+  typography: {
+    button: {
+      textTransform: 'none',
+    },
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>
 );

a5d9fb936fec25ea52530e54faa2a0be766bad19
Kota Hashihama <kotahashihama@gmail.com>
2024-08-11 21:57:14 +0900

    feat: プロンプトを調整

--- src/components/ExampleSection/ExampleImage/ExampleImage.tsx
+++ src/components/ExampleSection/ExampleImage/ExampleImage.tsx
@@ -16,7 +16,8 @@ export const ExampleImage = () => {
       return await openAi.images.generate({
         model: 'dall-e-3',
         prompt: `
-          次の例文を表現する愉快な画像を生成してください:
+          次の例文を表現する愉快な画像を生成してください
+          画像には吹き出しは不要です。描写のみですべてを表現するように努めてください。
           「${exampleSentence}」
         `,
         size: '1024x1024',
